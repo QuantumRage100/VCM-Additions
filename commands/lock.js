538124d3e62d269129aff082f33fc699cd9ceac5
QuantumRage100 <111104378+QuantumRage100@users.noreply.github.com>
2024-11-12 19:07:16 +1000

    Update lock.js
    discord.js 14.x.x and / commands

--- commands/lock.js
+++ commands/lock.js
@@ -1,113 +1,99 @@
+const { SlashCommandBuilder } = require('@discordjs/builders'); // For SlashCommandBuilder
+const { PermissionFlagsBits } = require('discord.js'); // For permission handling
 const utils = require('../utils.js');
+
 let votePending = {};
 
+// Function to lock the voice channel
 function doLock(voiceChannel) {
-    let everyone,
-        promise,
-        perms,
-        promises = [];
+    let perms, promises = [];
 
-    //Reset CONNECT overwrites
+    // Reset CONNECT overwrites
     perms = voiceChannel.permissionOverwrites.map(overwrite => ({
-        deny: overwrite.denied.remove('CONNECT').bitfield,
-        allow: overwrite.allowed.remove('CONNECT').bitfield,
+        deny: overwrite.denied.remove(PermissionFlagsBits.Connect).bitfield,
+        allow: overwrite.allowed.remove(PermissionFlagsBits.Connect).bitfield,
         id: overwrite.id,
         type: overwrite.type,
     }));
 
-    promise = voiceChannel.edit({
-        permissionOverwrites: perms
-    });
+    // Edit the permissions with the updated permissions
+    let promise = voiceChannel.edit({ permissionOverwrites: perms });
 
-    //Set CONNECT on for current members (and this bot)
+    // Set CONNECT on for current members (and this bot)
     promise = promise.then(() => {
         promises = [];
         voiceChannel.members.forEach(member => {
-            promises.push(voiceChannel.overwritePermissions(member, {
-                'CONNECT': true
-            }));
+            promises.push(voiceChannel.permissionOverwrites.create(member, { 'CONNECT': true }));
         });
-        promises.push(voiceChannel.overwritePermissions(voiceChannel.client.user, {
-            'CONNECT': true
-        }));
+        promises.push(voiceChannel.permissionOverwrites.create(voiceChannel.client.user, { 'CONNECT': true }));
         return Promise.all(promises);
     });
 
-    //Set CONNECT off for all roles
+    // Set CONNECT off for all roles
     promise = promise.then(() => {
         let promises = [];
-
-        voiceChannel.guild.roles.forEach(role => {
-            promises.push(voiceChannel.overwritePermissions(role, {
-                'CONNECT': false
-            }));
+        voiceChannel.guild.roles.cache.forEach(role => {
+            promises.push(voiceChannel.permissionOverwrites.create(role, { 'CONNECT': false }));
         });
-
         return Promise.all(promises);
     });
-
 
     return promise;
 }
 
 module.exports = {
-    name: 'lock',
-    description: 'Lock the voice channel so only the current occupants can join',
-    usage: '',
+    data: new SlashCommandBuilder()
+        .setName('lock')
+        .setDescription('Lock the voice channel so only the current occupants can join'),
     cooldown: 20,
     guildOnly: true,
 
-    execute(message, args) {
-        return new Promise((resolve, reject) => {
-            let subject = '',
-                voiceChannel = utils.get(message, 'member.voice.channel'),
-                targetUsers = [],
-                userCount,
-                user;
+    async execute(interaction) {
+        const voiceChannel = interaction.member.voice.channel;
 
-            if (voiceChannel == null) {
-                resolve('User not connected to a voice channel');
-                return;
+        if (!voiceChannel) {
+            return interaction.reply('You must be in a voice channel to use this command.');
+        }
+
+        if (voiceChannel.parentId !== interaction.channel.parentId) {
+            return interaction.reply('You cannot manage a channel in a different category.');
+        }
+
+        const userCount = voiceChannel.members.size;
+
+        if (userCount === 1) {
+            return interaction.reply('You are the only one here, so locking doesn\'t make sense.');
+        }
+
+        const targetUsers = [];
+        // Exclude bots from the vote
+        voiceChannel.members.forEach(member => {
+            if (member.id !== interaction.member.id && !member.user.bot) {
+                targetUsers.push(member);
             }
+        });
 
-            if (voiceChannel.parentID !== message.channel.parentID) {
-                resolve('Cannot manage the voice channel');
-                return;
+        if (votePending[voiceChannel.id] === true) {
+            return interaction.reply('There is already a vote pending on that channel.');
+        }
+
+        votePending[voiceChannel.id] = true;
+        
+        // Call vote system - Ensure that utils.vote works with interactions
+        utils.vote(`${interaction.user.tag} has requested to lock ${voiceChannel.name}. Please vote using the reactions below.`, interaction.channel, {
+            targetUsers,
+            time: 10000
+        }).then(results => {
+            if (((results.agree.count + 1) / userCount) > 0.5) { // +1 for requesting user
+                doLock(voiceChannel).then(() => {
+                    interaction.reply('Channel locked.');
+                });
+            } else {
+                interaction.reply('Request rejected by channel members.');
             }
-
-            userCount = voiceChannel.members.size;
-
-            if (userCount === 1) {
-                resolve('Why?');
-                return;
-            }
-
-            voiceChannel.members.forEach(member => {
-                if (member.id !== message.member.id) {
-                    targetUsers.push(member);
-                }
-            });
-
-            if (votePending[voiceChannel.id] === true) {
-                resolve('There is already a vote pending on that channel');
-                return;
-            }            
-            votePending[voiceChannel.id] = true;
-            utils.vote('Lock ' + voiceChannel.name + '? Please vote using the reactions below.', message.channel, {
-                targetUsers: targetUsers,
-                time: 10000
-            }).then(results => {
-                if (((results.agree.count + 1) / userCount) > 0.5) { //+1 for requesting user
-                    doLock(voiceChannel).then(() => {
-                        resolve('Channel locked');
-                    });
-                } else {
-                    resolve('Request rejected by channel members');
-                }
-                delete votePending[voiceChannel.id];
-            }).catch(() => {
-                delete votePending[voiceChannel.id];
-            });
+            delete votePending[voiceChannel.id];
+        }).catch(() => {
+            delete votePending[voiceChannel.id];
         });
     }
 };
