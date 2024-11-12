a30e97c64151ce470e682423d61d1f42851566c7
QuantumRage100 <111104378+QuantumRage100@users.noreply.github.com>
2024-11-12 17:09:00 +1000

    Update index.js
    updating for discord.js 14.x.x

--- index.js
+++ index.js
@@ -3,13 +3,22 @@
  *
  * Dynamic command handling based on https://github.com/discordjs/guide/tree/master/code_samples/command-handling/dynamic-commands
  */
-const Discord = require('discord.js');
+const { Client, GatewayIntentBits } = require('discord.js');
 require('dotenv').config();
 
 const commandListener = require('./listeners/command.js');
 const channelStateListener = require('./listeners/channelState.js');
 
-const client = new Discord.Client();
+// Initialize client with necessary intents
+const client = new Client({
+    intents: [
+        GatewayIntentBits.Guilds,                // Required for guild events
+        GatewayIntentBits.GuildMessages,         // Required if your bot needs to read messages
+        GatewayIntentBits.MessageContent,        // Required to access message content if needed
+        GatewayIntentBits.GuildVoiceStates,      // Required for managing voice states
+        GatewayIntentBits.GuildPresences         // Required to access user presence and activities
+    ]
+});
 
 client.once('ready', () => {
     commandListener.init(client);
