9171909964408e6e7c2bd728f5c8b5e88bbbee8a
QuantumRage100 <111104378+QuantumRage100@users.noreply.github.com>
2024-11-12 19:06:31 +1000

    Update boot.js
    updating to discord.js 14.x.x and getting ready for / commands

--- commands/boot.js
+++ commands/boot.js
@@ -1,77 +1,76 @@
+const { SlashCommandBuilder } = require('@discordjs/builders');
 const utils = require('../utils.js');
 let votePending = {};
 
 module.exports = {
-    name: 'boot',
-    description: 'Boot a user from the voice channel',
-    aliases: ['kick'],    
-    usage: '@user',
+    data: new SlashCommandBuilder()
+        .setName('boot')
+        .setDescription('Boot a user from the voice channel')
+        .addUserOption(option =>
+            option.setName('user')
+                .setDescription('The user to boot')
+                .setRequired(true)),
     cooldown: 120,
     guildOnly: true,
 
-    execute(message, args) {
-        return new Promise((resolve, reject) => {
-            let subject = '',
-                voiceChannel = utils.get(message, 'member.voice.channel'),
-                targetUsers = [],
-                userCount,
-                user;
+    async execute(interaction) {
+        let subject = '',
+            voiceChannel = interaction.member.voice.channel,
+            targetUsers = [],
+            userCount,
+            user = interaction.options.getUser('user');
 
-            if (voiceChannel == null) {
-                resolve('User not connected to a voice channel');
-                return;
+        if (!voiceChannel) {
+            return interaction.reply('User not connected to a voice channel');
+        }
+
+        if (voiceChannel.parentId !== interaction.channel.parentId) {
+            return interaction.reply('Cannot manage the voice channel');
+        }
+
+        if (!user) {
+            return interaction.reply('No user mentioned');
+        }
+
+        if (interaction.user.id === user.id) {
+            return interaction.reply('Why?');
+        }
+
+        userCount = voiceChannel.members.size;
+        voiceChannel.members.forEach(member => {
+            if (member.id !== interaction.member.id) {
+                targetUsers.push(member);
             }
+        });
 
-            if (voiceChannel.parentID !== message.channel.parentID) {
-                resolve('Cannot manage the voice channel');
-                return;
-            }
+        if (votePending[voiceChannel.id] === true) {
+            return interaction.reply('There is already a vote pending on that channel');
+        }
+        votePending[voiceChannel.id] = true;
 
-            if (!message.mentions || message.mentions.size === 0 || !(user = message.mentions.members.first())) {
-                resolve('No user mentioned');
-                return;
-            }
-
-
-            if (message.author.id === user.id) {
-                resolve('Why?');
-                return;
-            }
-
-            userCount = voiceChannel.members.size;
-            voiceChannel.members.forEach(member => {
-                if (member.id !== message.member.id) {
-                    targetUsers.push(member);
-                }
-            });
-
-            if (votePending[voiceChannel.id] === true) {
-                resolve('There is already a vote pending on that channel');
-                return;
-            }
-            votePending[voiceChannel.id] = true;
-            utils.vote(message.author.toString() + ' has requested that ' + user.toString() + ' be kicked from ' + voiceChannel.name + '? Please vote using the reactions below.', message.channel, {
-                targetUsers: targetUsers,
-                time: 10000
-            }).then(results => {
-                if (((results.agree.count + 1) / userCount) > 0.5) { //+1 for requesting user
-                    voiceChannel.overwritePermissions(user, {
-                        'CONNECT': false
-                    }).then(() => {
-                        let newChannel = voiceChannel.parent.children.find(channel => {
-                            return channel.type === 'voice' && channel.members.size === 0;
-                        });
+        utils.vote(interaction.user.toString() + ' has requested that ' + user.toString() + ' be kicked from ' + voiceChannel.name + '? Please vote using the reactions below.', interaction.channel, {
+            targetUsers: targetUsers,
+            time: 10000
+        }).then(results => {
+            if (((results.agree.count + 1) / userCount) > 0.5) { //+1 for requesting user
+                voiceChannel.permissionOverwrites.create(user, {
+                    'CONNECT': false
+                }).then(() => {
+                    let newChannel = voiceChannel.parent.children.find(channel => {
+                        return channel.type === 'voice' && channel.members.size === 0;
+                    });
+                    user.send('You have been removed from ' + voiceChannel.name).then(() => {
                         user.setVoiceChannel(newChannel).then(() => {
-                            resolve('User removed');
+                            interaction.reply('User removed');
                         });
                     });
-                } else {
-                    resolve('Request rejected by channel members');
-                }
-                delete votePending[voiceChannel.id];
-            }).catch(() => {
-                delete votePending[voiceChannel.id];
-            });
+                });
+            } else {
+                interaction.reply('Request rejected by channel members');
+            }
+            delete votePending[voiceChannel.id];
+        }).catch(() => {
+            delete votePending[voiceChannel.id];
         });
     }
 };
